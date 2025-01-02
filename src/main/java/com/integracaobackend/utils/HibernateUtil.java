1b9db16127f48f403143ddf58b33a11cd393d4b4
Lucas Sanderson dos Santos Passos <lucas.passos@eletra.local>
2025-01-02 08:52:11 -0300

    removing comments

--- src/main/java/com/integracaobackend/utils/HibernateUtil.java
+++ src/main/java/com/integracaobackend/utils/HibernateUtil.java
@@ -2,10 +2,6 @@ package com.integracaobackend.utils;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
